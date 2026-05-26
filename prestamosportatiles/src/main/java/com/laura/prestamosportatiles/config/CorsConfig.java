9c18db7d8c60db7864276a2f7e9b49c040c5a983
Laura Padilla <lpadillacobacho1@gmail.com>
2026-05-26 19:42:51 +0200

    arreglo cors

--- prestamosportatiles/src/main/java/com/laura/prestamosportatiles/config/CorsConfig.java
+++ prestamosportatiles/src/main/java/com/laura/prestamosportatiles/config/CorsConfig.java
@@ -2,19 +2,18 @@ package com.laura.prestamosportatiles.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-@EnableWebMvc
 public class CorsConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("https://prestamosdaw.vercel.app") 
+                .allowedOrigins("https://prestamosdaw-3xvg41899-tfg-prestamos.vercel.app")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
-                .allowCredentials(true);
+                .allowCredentials(true)
+                .maxAge(3600); // Esto ayuda a que el navegador cachee la respuesta de preflight
     }
 }
