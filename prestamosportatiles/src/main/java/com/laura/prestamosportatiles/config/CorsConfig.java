9e7e51f61d796f515b6ce812a0e80998c8af92c2
Laura Padilla <lpadillacobacho1@gmail.com>
2026-05-26 19:33:10 +0200

    actualizar config CORS

--- prestamosportatiles/src/main/java/com/laura/prestamosportatiles/config/CorsConfig.java
+++ prestamosportatiles/src/main/java/com/laura/prestamosportatiles/config/CorsConfig.java
@@ -1,23 +1,20 @@
 package com.laura.prestamosportatiles.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class CorsConfig {
+@EnableWebMvc
+public class CorsConfig implements WebMvcConfigurer {
 
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")
-                        .allowedOrigins("https://prestamosdaw.vercel.app")
-                        .allowedMethods("*")
-                        .allowedHeaders("*");
-            }
-        };
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("https://prestamosdaw.vercel.app") 
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
     }
 }
